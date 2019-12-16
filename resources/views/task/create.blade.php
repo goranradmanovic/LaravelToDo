c99b1a26a0000f9960dbfa55b2bf3f6e367f4507
Goran Radmanovic <goranradmanovic@gmail.com>
2019-12-16 09:28:37 +0100

    Small change to prop name

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -4,7 +4,7 @@
   <div class="container">
       <div class="row justify-content-center">
           <div class="col-md-8">
-            <createtask-component :userid="{{ auth()->user()->id }}"></createtask-component>
+            <createtask-component :user-id="{{ auth()->user()->id }}"></createtask-component>
           </div>
       </div>
   </div>
